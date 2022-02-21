bc46705f6f98980410e480f8b0b38ac581173f6a
Stefan Schmidt <snst@meek.de>
2022-02-21 22:51:00 +0000

    update

--- scripts/buildroot/4_make_savedefconfig.sh
+++ scripts/buildroot/4_make_savedefconfig.sh
@@ -3,8 +3,4 @@ source "${SCRIPT_DIR}/../build_env.sh"
 
 cd ${BUILDROOT_DIR}
 
-make 2>&1 | tee build.log
-
-sudo rm -rf ${NFSROOT_DIR}/*
-
-sudo tar -C ${NFSROOT_DIR} -xf ${BUILDROOT_DIR}/output/images/rootfs.tar
+make savedefconfig
--- scripts/buildroot/7_get_data.sh
+++ scripts/buildroot/7_get_data.sh
@@ -1,6 +1,6 @@
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 source "${SCRIPT_DIR}/../build_env.sh"
 
-rm -rf "${SCRIPT_DIR}/data/${BBBL_OVERLAY}"
+#rm -rf "${SCRIPT_DIR}/data/${BBBL_OVERLAY}"
 cp -v -r "${BUILDROOT_OVERLAY_DIR}/${BBBL_OVERLAY}" ${SCRIPT_DIR}/data
 cp -v "${BUILDROOT_DEFCONFIG_DIR}/${BBBL_DEFCONFIG}" ${SCRIPT_DIR}/data
